d28ab278077e0d764c9f1075af3f8defdfcb0ec0
Mikhail Loginov <mloginov@codio.com>
2020-02-04 15:07:58 +0300

    added styles added webpack build

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+// @ts-ignore
+import $ from 'cash-dom';
+
 export const draw = (selector: string, jsonString: string): void => {
+  const container = $(selector);
+  console.log(container);
   console.log(selector);
   console.log(jsonString);
 };
 
 export const getJsonData = (): string => {
   return '{}';
-}
+};
---
+++ src/types/index.d.ts
@@ -0,0 +1,5 @@
+declare namespace ParsonsUI {
+  function draw(selector: string, jsonString: string): void;
+  function getJsonData(): string;
+}
+
