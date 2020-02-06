b141e987164f4efde7974e2f2c9acbfcba3f055b
Mikhail Loginov <mloginov@codio.com>
2020-02-06 17:04:06 +0300

    added dev server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import ParsonsUI from './lib/ParsonsUI'
 import { ParsonsSettings } from './@types/ParsonsSettingsInterface'
+import './style.less'
 
 export const build = (selector: string, settings: ParsonsSettings): ParsonsUI => new ParsonsUI(selector, settings)
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   mode: 'development',
   entry: './src/index.ts',
   // devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
   module: {
     rules: [
       {
@@ -12,11 +19,34 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.(le|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: !isProduction
+            },
+          },
+          'css-loader',
+          'less-loader'
+        ],
+      }
     ],
   },
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ],
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: !isProduction ? '[name].css' : '[name].[hash].css',
+      chunkFilename: !isProduction ? '[id].css' : '[id].[hash].css',
+    })
+  ],
   output: {
     library: 'ParsonsUI',
     libraryTarget: 'umd',
