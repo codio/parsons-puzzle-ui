38980ffabe3afc42372a651979da9369e28f8688
Mikhail Loginov <mloginov@codio.com>
2020-02-27 08:27:09 +0300

    fixed test page style remove dist folder from excluded

---
+++ dist/lib/render.d.ts
@@ -0,0 +1,11 @@
+import { Cash } from 'cash-dom';
+import { ParsonsGrader, ParsonsSettings, UnitTest, VariableTest } from '../@types/types';
+export declare const renderVarTest: (test?: VariableTest | undefined) => Cash;
+export declare const renderUnitTest: (test?: UnitTest | undefined) => Cash;
+export declare const renderGrader: (container: Cash, grader: ParsonsGrader) => void;
+export declare const render: (container: Cash, settings: ParsonsSettings) => void;
+declare const _default: {
+    render: (container: Cash, settings: ParsonsSettings) => void;
+};
+export default _default;
+//# sourceMappingURL=render.d.ts.map
