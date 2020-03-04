04388cec5b066f20e4b236b7bd43d8b1a789ad12
achetporov <achetporov@codio.com>
2020-03-04 14:16:25 +0300

    small changes

---
+++ dist/lib/ParsonsUI.d.ts
@@ -0,0 +1,10 @@
+import { ParsonsSettings } from '../@types/types';
+export default class ParsonsUI {
+    private readonly initialSettings;
+    private readonly container;
+    constructor(selector: string, initialSettings: ParsonsSettings);
+    private render;
+    private bindEvents;
+    export(): ParsonsSettings;
+}
+//# sourceMappingURL=ParsonsUI.d.ts.map
