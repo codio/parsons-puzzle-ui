7bcf75561a6fad466900353595ce3927c6ecebaf
Mikhail Loginov <mloginov@codio.com>
2020-03-09 10:57:45 +0300

    updated build

--- dist/lib/data-helper.d.ts
+++ dist/lib/data-helper.d.ts
@@ -1,5 +1,7 @@
 import { Cash } from 'cash-dom';
-import { ParsonsOptions, ParsonsSettings } from '../@types/types';
+import { ParsonsOptions, ParsonsSettings, UnitTest, VariableTest } from '../@types/types';
+export declare const collectVariableTest: (container: Cash) => VariableTest;
+export declare const collectUnitTest: (container: Cash) => UnitTest;
 export declare const collectData: (container: Cash, initialOptions: ParsonsOptions) => ParsonsSettings;
 declare const _default: {
     collectData: (container: Cash, initialOptions: ParsonsOptions) => ParsonsSettings;
