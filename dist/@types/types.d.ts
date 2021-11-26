409fedb2a827d78e4a8041475ab48aac3f83652a
sergei-bronnikov <48258384+sergei-bronnikov@users.noreply.github.com>
2021-11-26 10:32:26 +0300

    parsons feedback (#44)

--- dist/@types/types.d.ts
+++ dist/@types/types.d.ts
@@ -40,6 +40,7 @@ export interface ParsonsOptions {
     x_indent?: number;
     exec_limit?: number;
     unittest_code_prepend?: string;
+    show_feedback?: boolean;
 }
 export interface ParsonsSettings {
     initial: string;
--- dist/lib/converters.d.ts
+++ dist/lib/converters.d.ts
@@ -1,5 +1,5 @@
 import { ParsonsGrader, UnitTest } from '../@types/types';
-export declare const convertParsonsGraderFuncToEnum: (grader?: string | (() => void) | undefined) => ParsonsGrader;
+export declare const convertParsonsGraderFuncToEnum: (grader?: (() => void) | string | undefined) => ParsonsGrader;
 export declare const convertTestVariablesToString: (variables: object) => string;
 export declare const convertUnitTestsFromString: (unitTestsStr: string | undefined) => UnitTest[];
 declare const _default: {
